aa4e2e02d3bddf1a7f20dd89f881f0153c8a6587
Hamza <61266184+ihamzazam@users.noreply.github.com>
2023-02-15 13:42:16 +0800

    Update deployProxy.js

--- scripts/deployProxy.js
+++ scripts/deployProxy.js
@@ -1,4 +1,5 @@
 const { ethers, upgrades } = require("hardhat");
+const fs = require('fs');
 
 async function main() {
   const BoxV1 = await ethers.getContractFactory("BoxV1");
@@ -6,6 +7,7 @@ async function main() {
   await proxy.deployed();
 
   console.log('proxy address: ' + proxy.address);
+  fs.writeFileSync('proxy_address.txt', proxy.address);
 }
 
 main();
