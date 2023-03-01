aacd0ed2df9e922391f0f7fb5c15b2ec069b740a
Hamza <61266184+ihamzazam@users.noreply.github.com>
2023-03-01 17:09:44 +0800

    Update upgrade.js

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,18 +1,25 @@
-const { ethers, upgrades } = require("hardhat");
-
-// TO DO: Place the address of your proxy here!
-const proxyAddress = "0x98344cc29FCA9fEd7dD27e050aF16a76Df4f73DC";
+const { ethers } = require("hardhat");
+const { upgrades } = require("hardhat");
+require('@openzeppelin/hardhat-upgrades');
 
 async function main() {
+  const proxyAddress = '0x811f33b3DD0de05A5e0f575C087Bd3E5f673Cbe7';
+  console.log(`Original proxy address: ${proxyAddress}`);
+
   const BoxV2 = await ethers.getContractFactory("BoxV2");
-  const upgraded = await upgrades.upgradeProxy(proxyAddress, BoxV2);
-  console.log((await upgraded.area()).toString());
-  console.log((await upgraded.perimeter()).toString());
+  console.log("Upgrading to BoxV2...");
+
+  const boxV2 = await upgrades.upgradeProxy(proxyAddress, BoxV2);
+  console.log(`BoxV2 Proxy address: ${boxV2.address}`);
+
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+  console.log(`Implementation address: ${implementationAddress}`);
+
+  const adminAddress = await upgrades.admin.getInstance();
+  console.log("ProxyAdmin :", adminAddress.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
