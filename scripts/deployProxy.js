d61181bd138101c8ec771b4cedeecdb012485731
Hamza <61266184+ihamzazam@users.noreply.github.com>
2023-03-01 17:33:45 +0800

    Update deployProxy.js

--- scripts/deployProxy.js
+++ scripts/deployProxy.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat");
 const { upgrades } = require("hardhat");
 
 async function main() {
-      const boxAddress = "0xDb83A481a472A82543e338e44440D1b6A4E0ffD2"; // replace with the address of your Box contract
+      const boxAddress = "0xdfc66ee2eeE42123e401E6B27d61769E412c9744"; // replace with the address of your Box contract
       const Box = await ethers.getContractFactory("BoxV1");
       const box = await Box.attach(boxAddress);
     
