2bf72203899e0f512db39a607c02ccf8d5d396da
Hamza <hamzazam00@gmail.com>
2023-03-01 07:16:40 +0000

    Update proxy address

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat");
 
 // TO DO: Place the address of your proxy here!
-const proxyAddress = "0x576965d47D4e7D052f91CfaF8eFDCA81E649a892";
+const proxyAddress = "0xAd8A123F78F2372f88f8EfD1EFC25DB5d3E3940c";
 
 async function main() {
   const BoxV2 = await ethers.getContractFactory("BoxV2");
