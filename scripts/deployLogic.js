e7cfc2de49793f5b1c73cfb68717f8ef4d795b40
Hamza <61266184+ihamzazam@users.noreply.github.com>
2023-03-01 17:07:29 +0800

    Update and rename deployProxy.js to deployLogic.js

---
+++ scripts/deployLogic.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Deploy Implementation Contract
+  const Box = await ethers.getContractFactory("Box");
+  const box = await Box.deploy();
+  await box.deployed();
+
+  console.log("Contract deployed to:", box.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+});
